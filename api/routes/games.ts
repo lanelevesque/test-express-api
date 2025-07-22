f0d246940e89216daa7ebbc63c78dd3f1d6ddda2
Lane Levesque <lane.levesque@hotmail.com>
2025-07-21 19:56:43 -0700

    added games endpoint

---
+++ api/routes/games.ts
@@ -0,0 +1,22 @@
+import { UUID } from "crypto";
+
+export {};
+
+const express = require("express");
+const router = express.Router();
+const supabase = require("../supabaseClient");
+
+router.get("/", (req: any, res: any) => {
+  res.send("this is the games router.");
+});
+
+router.get("/:userId", async (req: any, res: any) => {
+  const userId: UUID = req.params.userId;
+  const { data, error } = await supabase
+    .from("games")
+    .select()
+    .eq("user_id", userId);
+  res.send(data);
+});
+
+module.exports = router;
