d9835093cae800732c7a3a19828e9be0276f08f6
Lane Levesque <lane.levesque@hotmail.com>
2025-07-21 19:58:22 -0700

    forgot to register router

--- api/index.ts
+++ api/index.ts
@@ -3,9 +3,11 @@ const app = express();
 
 const usersRouter = require("./routes/users");
 const productsRouter = require("./routes/products");
+const gamesRouter = require("./routes/games");
 
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
+app.use("/games", gamesRouter);
 
 app.get("/", (req: any, res: any) => res.send("Express on Vercel!"));
 
