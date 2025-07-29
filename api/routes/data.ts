a5816f2f1309b26f268f56886c6137e6fc7776e3
Lane Levesque <lane.levesque@hotmail.com>
2025-07-28 22:01:33 -0700

    fixing again

--- api/routes/data.ts
+++ api/routes/data.ts
@@ -9,7 +9,7 @@ const supabase = require("../clients/supabaseClient");
 router.get("/", async (req: any, res: any) => {
   const { data, error } = await supabase
     .from("igdb_games")
-    .select("id", "updated_at");
+    .select("id, updated_at");
   res.send(data);
 });
 
